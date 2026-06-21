3a2cd1b266b20b8568afbfe8f1d0e81900070dc8
Fortune Emmanuel <fortuneemmanuel18@gmail.com>
2026-06-21 04:27:01 +0100

    done with the About Page

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/home/HomePage'
 import { ProductsPage } from './pages/products/ProductsPage'
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
+import { AboutPage } from './pages/about/AboutPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
     </>
   )
---
+++ E-commerce/src/pages/about/AboutPage.jsx
@@ -0,0 +1,29 @@
+import { NavBar } from '../../components/NavBar'
+import { Footer } from '../../components/Footer'
+import styles from './AboutPage.module.css'
+
+export function AboutPage() {
+    return (
+        <>
+            <NavBar />
+            <div className={styles.aboutWrapper}>
+
+                {/* Hero Section */}
+                <div className={styles.aboutHero}>
+                    <div className={styles.aboutHeroContent}>
+                        <span className={styles.badge}>Founded June 14, 2026</span>
+                        <p className={styles.aboutSubtitle}>
+                            At Nexus Gadgets, we are committed to providing quality smartphones, laptops, and tech accessories at competitive prices. We focus on delivering genuine products, reliable customer service, and a seamless shopping experience. Whether you’re looking for the latest iPhone, MacBook, or everyday tech essentials, we’re here to help you stay connected with the technology you need.
+                        </p>
+                    </div>
+                    <div className={styles.aboutHeroImage}>
+                        {/* Add your image here */}
+                        <img src="images/Gadget-Hub.jpg" alt="Nexus Gadgets" className={styles.aboutImage} />
+                    </div>
+                </div>
+
+            </div>
+            <Footer />
+        </>
+    )
+}
